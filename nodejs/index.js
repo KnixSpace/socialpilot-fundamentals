353d1337e8d69a11f02f048d60b22c90bedeb0dc
Krupal Patel <krupalgp2003@gmail.com>
2025-01-24 09:17:49 +0530

    Refactor server implementation to read package.json and return JSON response

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,14 +1,18 @@
 let http = require("http");
+let fs = require("fs");
 let url = require("url");
-require("./fs");
+// require("./fs");
+require("./test");
 
-// http
-//   .createServer(function (req, res) {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.write("Hello world");
-//     res.end();
-//   })
-//   .listen(3000);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/json" });
+    fs.readFile("package.json", (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(3000);
 
 //clientServer method is use to create server in node js
 
