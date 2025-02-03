909fe5fbd3cbe09c69268f6af963994edc4200e5
Krupal Patel <krupalgp2003@gmail.com>
2025-02-03 11:48:49 +0530

    Add email and password validation functions with utility checks

--- koa/todo/validators/miscellaneous.js
+++ koa/todo/validators/miscellaneous.js
@@ -1,17 +1,59 @@
-const result = (property, error = null) => {
-  return {
-    property,
-    error,
-  };
+const { buildPropertyError } = require("./validator");
+
+const isEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (typeof email === "string" && emailRegex.test(email.trim())) {
+    return true;
+  }
+  return false;
 };
 
-const isEmail = (data, property) => {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (typeof data[property] !== "string") {
-    return result(property, "Email must be string");
-  } else if (!emailRegex.test(data[property].trim())) {
-    return result(property, "Not an valid email");
+const isPassword = (password, property, errors) => {
+  if (!isValidType(password, "string")) {
+    errors.push(buildPropertyError(property, "Must be an valid password"));
+    return;
+  } else if (password.trim().length < 8 || password.trim().length > 16) {
+    errors.push(
+      buildPropertyError(property, "Must be of 8 to 16 character long")
+    );
+    return;
   }
 };
 
-module.exports = { isEmail };
+const isValidType = (data, type) => {
+  if (typeof data === type) {
+    return true;
+  }
+  return false;
+};
+
+const isDefined = (data) => {
+  if (data !== undefined) {
+    return true;
+  }
+  return false;
+};
+
+const isMin = (data, min) => {
+  if (data?.trim().length >= min) {
+    return true;
+  }
+  return false;
+};
+
+const isMax = (data, max) => {
+  if (data?.trim().length <= max) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = {
+  buildPropertyError,
+  isEmail,
+  isPassword,
+  isValidType,
+  isDefined,
+  isMin,
+  isMax,
+};
