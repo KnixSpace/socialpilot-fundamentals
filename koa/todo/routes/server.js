e480c77f9bb7d9bd5cfdb2b69aa8b2ed2b73ef31
Krupal Patel <krupalgp2003@gmail.com>
2025-02-03 11:52:54 +0530

    Remove deprecated todo JSON file and add properties endpoint to server routes

--- koa/todo/routes/server.js
+++ koa/todo/routes/server.js
@@ -5,4 +5,15 @@ router.get("/", (ctx) => {
   ctx.body = "Infinix is alive";
 });
 
+router.get("/properties/:id", (ctx) => {
+  ctx.body = {
+    origin: ctx.origin,
+    originalUrl: ctx.originalUrl,
+    url: ctx.url,
+    idempotent: ctx.idempotent,
+    ip: ctx.ip,
+    state: ctx.state,
+  };
+});
+
 module.exports = router;
