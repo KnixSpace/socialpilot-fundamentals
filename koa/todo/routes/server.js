e567b70f98ee4bde096a8fea4f2a3133297bcadc
Krupal Patel <krupalgp2003@gmail.com>
2025-02-11 17:04:26 +0530

    Remove API versioning prefix from Koa router

--- koa/todo/routes/server.js
+++ koa/todo/routes/server.js
@@ -1,5 +1,5 @@
 const Router = require("@koa/router");
-const router = new Router({ prefix: "/api/v1" });
+const router = new Router();
 
 router.get("/", (ctx) => {
   ctx.body = "Infinix is alive";
