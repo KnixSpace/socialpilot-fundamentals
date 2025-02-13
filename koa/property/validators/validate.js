008692c9e449f0a0c25a525e8b021a03b895498d
Krupal Patel <krupalgp2003@gmail.com>
2025-02-13 11:35:17 +0530

    Remove timing logs from property validation; streamline error handling process

--- koa/property/validators/validate.js
+++ koa/property/validators/validate.js
@@ -5,9 +5,7 @@ const buildPropertyError = (property, error) => ({ property, error });
 const validate = (validators) => async (ctx, next) => {
   const errors = [];
   await Promise.each(validators, async (validator) => {
-    console.time("name");
     await validator(ctx, errors);
-    console.timeEnd("name");
   });
   if (errors.length > 0) {
     ctx.status = 400;
