1048c1cb5de6d949ac2738014a8bc0de2e198033
Krupal Patel <krupalgp2003@gmail.com>
2025-02-14 13:09:39 +0530

    Rename user data handling functions for clarity; update user creation and retrieval methods

--- koa/property/db/user.js
+++ koa/property/db/user.js
@@ -3,14 +3,14 @@ const userCollection = client.db(process.env.DB_NAME).collection("user");
 
 const isUser = async (filters) => await userCollection.countDocuments(filters);
 
-const saveUser = async (userData) => await userCollection.insertOne(userData);
+const createUser = async (userData) => await userCollection.insertOne(userData);
 
-const getUser = async (filters, options) =>
+const readUser = async (filters, options) =>
   await userCollection.findOne(filters, options);
 
-const updateUser = async (filters, data) => {
-  data.updatedOn = new Date();
-  return await userCollection.updateOne(filters, { $set: data });
-};
+const updateUser = async (filters, data) =>
+  await userCollection.updateOne(filters, {
+    $set: { ...data, updatedOn: new Date() },
+  });
 
-module.exports = { isUser, saveUser, getUser, updateUser };
+module.exports = { isUser, createUser, readUser, updateUser };
