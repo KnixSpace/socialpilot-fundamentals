3b370c1b18370d82564064cfad5721665810b8a4
Krupal Patel <krupalgp2003@gmail.com>
2025-02-14 13:10:03 +0530

    Add global error handling middleware to Koa app; return 500 status on errors

--- koa/property/config/koa.js
+++ koa/property/config/koa.js
@@ -6,6 +6,15 @@ const routes = require("../routes/index");
 
 app.use(parser());
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: error.message } || "error occured";
+  }
+});
+
 routes(app);
 
 module.exports = app;
