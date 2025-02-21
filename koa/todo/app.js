3894df918816cb24018b3a8440df402da532e216
Krupal Patel <krupalgp2003@gmail.com>
2025-02-21 13:17:28 +0530

    Add deployment check comment in app.js

--- koa/todo/app.js
+++ koa/todo/app.js
@@ -1,6 +1,7 @@
 const { connectDB } = require("./config/database");
 const koaApp = require("./config/koa");
 
+//just checking deployment is off for this branch
 const startApp = async () => {
   try {
     await connectDB();
